c90e9b9dba80716673fe05fa3c5e94461d60a545
leonardo Nunes Freitas <leonardonunesfreitas1@gmail.com>
2022-06-12 13:38:45 -0300

    treinando

---
+++ Alura-chalange/on-day.js
@@ -0,0 +1,25 @@
+
+var numeroUm = 1
+var stringUm = '1'
+var numeroTrinta = 30
+var stringTrinta = '30'
+var numeroDez = 10
+var stringDez = '10'
+
+if ( numeroUm == stringUm) {
+  console.log('tem o mesmo valor  e tipos diferente')
+} else {
+  console.log('não tem o mesmo valor')
+}
+
+if (numeroTrinta === stringTrinta) {
+  console.log('mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis não tem o mesmo valor')
+}
+
+if (numeroDez == stringDez) {
+  console.log('As variáveis  tem o mesmo valor e tipos diferentes')
+} else {
+  console.log('As variáveis não tem o mesmo valor')
+}
